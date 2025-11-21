fa01d7fee57bb428dc932b7b12a7eea1b07d2e55
ammu8147 <amulyaej003@gmail.com>
2025-11-21 10:53:28 +0530

    Add files via upload

---
+++ arr_fun.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int addarr (int arr[], int size)
+{
+    int result = 0;
+    for (int i = 0; i<size; i++)
+    {
+        result=result+arr[i];
+    }
+    return result;
+}
+void main()
+{
+    int arr[3]={78,39,56};
+    int size=3;
+    printf("%d\n",addarr(arr,size));
+}
